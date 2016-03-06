6e0a22414b6306fb7362661ba08a33faca7b94bd
Smith Dhumbumroong <zodmaner@gmail.com>
2016-03-07 02:10:08 +0700

    Exports functions and methods for public consumption

--- package.lisp
+++ package.lisp
@@ -1,5 +1,21 @@
 ;;;; package.lisp
 
 (defpackage #:trivial-openstack
-  (:use #:cl))
+  (:use #:cl)
+  (:export :cdr-assoc
+           :authenticate
+           :list-images
+           :list-flavors
+           :list-servers
+           :list-servers-detail
+           :create-server
+           :delete-server
+           :list-floating-ips
+           :create-floating-ip
+           :associate-floating-ip
+           :list-default-security-group-rules
+           :create-default-security-group-rule
+           :add-security-rule-accept-all-icmp
+           :add-security-rule-accept-all-tcp
+           :add-security-rule-accept-all-udp))
 
