33d7f8ba64871d76eaa03ccab6b1bbf2c4bb513b
Smith Dhumbumroong <zodmaner@gmail.com>
2016-05-15 00:02:07 +0700

    Minor refactor

--- package.lisp
+++ package.lisp
@@ -2,31 +2,31 @@
 
 (defpackage #:trivial-openstack
   (:use #:cl)
-  (:export :get-value
-           :with-openstack-response
-           :def-openstack-api
-           ;; Keystone API bindings
-           :*service-catalog*
-           :get-public-url
-           :*openstack-token*
-           :token
-           :token-expiration-time
-           :authenticate
-           ;; Glance API bindings
-           :list-images
-           ;; Nova API bindings
-           :list-flavors
-           :list-flavor-details
-           :list-servers
-           :list-servers-details
-           :create-server
-           :delete-server
-           :list-floating-ips
-           :create-floating-ip
-           :associate-floating-ip
-           :list-default-security-group-rules
-           :create-default-security-group-rule
-           :add-security-rule-accept-all-icmp
-           :add-security-rule-accept-all-tcp
-           :add-security-rule-accept-all-udp))
+  (:export #:get-value
+           #:with-openstack-response
+           #:def-openstack-api
+           #;; Keystone API bindings
+           #:*service-catalog*
+           #:get-public-url
+           #:*openstack-token*
+           #:token
+           #:token-expiration-time
+           #:authenticate
+           #;; Glance API bindings
+           #:list-images
+           #;; Nova API bindings
+           #:list-flavors
+           #:list-flavor-details
+           #:list-servers
+           #:list-servers-details
+           #:create-server
+           #:delete-server
+           #:list-floating-ips
+           #:create-floating-ip
+           #:associate-floating-ip
+           #:list-default-security-group-rules
+           #:create-default-security-group-rule
+           #:add-security-rule-accept-all-icmp
+           #:add-security-rule-accept-all-tcp
+           #:add-security-rule-accept-all-udp))
 
