7a1bae340a30917a25ef9b0c3cc95b081d4bf94d
Smith Dhumbumroong <zodmaner@gmail.com>
2016-06-23 23:49:41 +0700

    Refactors with-openstack-response to use recursion to generate http-request arguments
    Also use gensym to properly generate symbols for various variables in
    the macro.

--- trivial-openstack.lisp
+++ trivial-openstack.lisp
@@ -24,11 +24,11 @@ other alists nested inside."
         (apply #'get-value new-alist (cdr keys)))
       (assoc-value alist (car keys) :test #'string=)))
 
-(defmacro with-openstack-response (stream (uri http-method &optional x-auth-token content)
+(defmacro with-openstack-response (response-stream (uri http-method &optional x-auth-token content)
                                    &body body)
   "Sends an API request to an OpenStack endpoint at URI and binds a
 stream of the response body that is returned by an OpenStack service
-to a user specified stream symbol.
+to a user specified response stream symbol.
 
 If the returned stream has zero length, then NIL will be bound to the
 stream symbol. If an error status code is returned, then the function
@@ -36,28 +36,47 @@ will throw an error.
 
 The x-auth-token optional argument can be used to send the
 authentication token to the endpoint, while the content optional
-argument can be used to send any content with the request."
-  (let ((lambda-list (list :method http-method)))
-    (when x-auth-token
-      (push `(list (cons "X-Auth-Token" ,x-auth-token)) lambda-list)
-      (push :additional-headers lambda-list))
-    (when content
-      (push content lambda-list)
-      (push :content lambda-list))
-    `(multiple-value-bind (response-body-stream status-code headers uri
-                                                stream must-close reason-phase)
-         (drakma:http-request ,uri
-                              :content-type "application/json"
-                              ,@lambda-list
-                              :want-stream t)
-       (declare (ignore headers uri stream must-close))
-       (if (or (= status-code 200)
-               (= status-code 202)
-               (= status-code 204))
-           (when (not (null (flexi-streams:peek-byte response-body-stream nil nil nil)))
-             (let ((,stream response-body-stream))
-               ,@body))
-           (error (format nil "Error code ~A, ~A." status-code reason-phase))))))
+argument can be used to send additional content (usually a JSON) along
+with the request."
+  (labels ((gen-http-request-args (args x c)
+             (cond
+               ((null args) (cond
+                              (x (cons :additional-headers
+                                       (cons (list (cons "X-Auth-Token" x))
+                                             (gen-http-request-args args nil c))))
+                              (c (cons :content
+                                       (cons c
+                                             (gen-http-request-args args x nil))))
+                              (t '())))
+               (t (cons (car args)
+                        (gen-http-request-args (cdr args) x c))))))
+    (let* ((initial-args (list uri
+                               :want-stream t
+                               :content-type "application/json"
+                               :method http-method))
+           (http-request-args (gen-http-request-args initial-args x-auth-token content))
+           (response-body-stream (gensym "W-OS-"))
+           (status-code (gensym "W-OS-"))
+           (headers (gensym "W-OS-"))
+           (uri (gensym "W-OS-"))
+           (stream (gensym "W-OS-"))
+           (must-close (gensym "W-OS-"))
+           (reason-phase (gensym "W-OS-")))
+      `(multiple-value-bind (,response-body-stream
+                             ,status-code
+                             ,headers
+                             ,uri
+                             ,stream
+                             ,must-close
+                             ,reason-phase) (drakma:http-request ,@http-request-args)
+         (declare (ignore ,headers ,uri ,stream ,must-close))
+         (if (or (= ,status-code 200)
+                 (= ,status-code 202)
+                 (= ,status-code 204))
+             (when (not (null (flexi-streams:peek-byte ,response-body-stream nil nil nil)))
+               (let ((,response-stream ,response-body-stream))
+                 ,@body))
+             (error (format nil "Error code ~A, ~A." ,status-code ,reason-phase)))))))
 
 (defmacro def-openstack-api (name lambda-list
                              (stream http-method uri-list &optional json os-keystone)
